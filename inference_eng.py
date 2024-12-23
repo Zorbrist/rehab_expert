9e23903cf9b9a0fdb26f41416b63757554886b9a
Zorbrist <145312660+Zorbrist@users.noreply.github.com>
2024-12-24 00:58:57 +0800

    inference change

--- inference_eng.py
+++ inference_eng.py
@@ -1,14 +1,13 @@
 # inference_eng.py
 
 class InferenceEngine:
-    def __init__(self, rules, facts):
+    def __init__(self, rules):
         self.rules = rules
-        self.facts = facts
 
-    def infer(self):
+    def infer(self, user_facts):
         recommendations = []
         for rule in self.rules:
             # Check if all conditions in the rule are met
-            if all(condition in self.facts for condition in rule["if"]):
+            if all(condition in user_facts for condition in rule["if"]):
                 recommendations.append(rule["then"])
         return recommendations
